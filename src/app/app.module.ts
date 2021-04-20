5df394d754ce3f76357465e3f7d6cda5ad190b21
Jennifer Halvorson <jenhalv06@gmail.com>
2021-04-20 09:34:44 -0500

    list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonUnitComponent
+    InputButtonUnitComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
